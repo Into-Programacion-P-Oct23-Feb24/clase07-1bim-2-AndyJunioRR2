05c772bf8494694cd9592e163508613368f6d4ee
AndyJunioRR2 <andyvein20@gmail.com>
2023-11-23 16:12:00 -0500

    Ejercicios en clase

--- EjemplosDoWhile/Ejemplos/src/ejemplos01/Ejemplo05.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos01/Ejemplo05.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ Utiliza la potencia como limite en el contador, luego se multiplica por el numero
+luego el resultado toma un nuevo valor y se vuelve a realizar la misma multiplicacion
+dando un nuevo valor, este ciclo se sigue repitiendo hasta que el contador sea igual
+a la potencia
  */
 package ejemplos01;
 
@@ -12,6 +13,8 @@ package ejemplos01;
 public class Ejemplo05 {
 
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
         // 2. numero, i[0, n]
         int numero;
         // 3. potencia, i[0, n]
@@ -19,9 +22,9 @@ public class Ejemplo05 {
         // 6. resultado, i[0, n]
         int resultado;
         // numero <-- 4
-        numero = 4;
+        numero = 3;
         // 8. potencia <-- 3
-        potencia = 3;
+        potencia = 2;
         // 9. resultado <-- 1
         resultado = 1;
 
