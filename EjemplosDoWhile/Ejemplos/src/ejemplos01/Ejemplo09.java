6a28093dbcd694a940d31ee93a01fa858ab55ffa
AndyJunioRR2 <andyvein20@gmail.com>
2023-11-23 17:08:03 -0500

    Ejercicios en clase

--- EjemplosDoWhile/Ejemplos/src/ejemplos01/Ejemplo09.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos01/Ejemplo09.java
@@ -52,6 +52,7 @@ public class Ejemplo09 {
 
         // promedio = suma / contador_calificaciones;
         promedio = (double) suma / contador_calificaciones;
+        // se pone (double) para poder transformar la suma de un valor entero a un valor double
         cadenaFinal = String.format("%s\nPromedio de calificaciones: %.2f",
                 cadenaFinal,
                 promedio);
